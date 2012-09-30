fda5adc5780da19fa09fcfb2ba1195ed879c2c4e
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2012-09-30 20:29:07 +0300

    a) paredit-mode in markdown-mode b) .gitignore

--- kits/markdown-kit.el
+++ kits/markdown-kit.el
@@ -1,5 +1,8 @@
 (autoload 'markdown-mode "markdown-mode" "Start markdown-mode" t)
 
+(defun turn-on-paredit () (paredit-mode 1))
+
+(add-hook 'markdown-mode-hook 'turn-on-paredit)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
 (provide 'markdown-kit)
